0161817a8436ec70039bd06e92a3284a53eafe19
macdevmin <macdevmin@gmail.com>
2025-08-14 04:28:14 +0200

    Fix webpack paths for Vercel deployment

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.tsx'),
+  entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
@@ -34,7 +34,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
+      template: './public/index.html',
       filename: 'index.html',
     }),
   ],
